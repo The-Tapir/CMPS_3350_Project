60df94c23dfa6c667f60185e72adb0b4411569b8
Nathan Wardinsky <117780147+The-Tapir@users.noreply.github.com>
2023-09-27 09:21:40 -0700

    work in progress for timer in game

---
+++ nwardinsky.cpp
@@ -0,0 +1,30 @@
+//Program: Jerry the Race Car Driver
+//Author: Nathan Wardinsky
+//Date started: 9/25/2023
+#include"nwardinsky.h"
+
+int timer() {
+    time_t current_time;
+
+    time(&current_time);
+
+    return current_time;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ nwardinsky.h
@@ -0,0 +1,27 @@
+//Only definitions or prototypes
+//No executables
+#ifndef _nwardinsky_H_
+#define _nwardinsky_H_
+
+#include <iostream>
+#include <ctime>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
