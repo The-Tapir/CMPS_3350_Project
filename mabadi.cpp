5b2c285be7b77ee517f6e87dbd58b41f032090f9
Mostafa Abadi <mabadi@odin.cs.csubak.edu>
2023-11-18 21:53:34 -0800

    All files to implement the racetrack

--- mabadi.cpp
+++ mabadi.cpp
@@ -1,36 +1,71 @@
 //Name: Mostafa Abadi
 //Created: September 26th, 2023
-
-
-
-#include <X11/Xlib.h>
-#include <GL/gl.h>
-#include <GL/glx.h>
-#include <GL/glu.h>
 #include "mabadi.h"
+// Required for the time functions
+//#include <chrono>
+using namespace std::chrono;  // This allows easier access to the chrono functions
+// Function to load an OBJ file
+int faceIndex = 0; // Variable to keep track of face index
+OBJMesh loadOBJ(const std::string& racetrack) {
+    OBJMesh mesh;
+    //faceIndex();    
+    std::vector<Vertex> vertices;
+    std::ifstream file(racetrack);
+    if (!file.is_open()) {
+        return mesh;
+    }
+    std::string line;
+    while (std::getline(file, line)) {
+        std::istringstream iss(line);
+        std::string token;
+        iss >> token;
 
+        if (token == "v") {
+            // Vertex position
+            Vertex vertex;
+            iss >> vertex.position[0] >> vertex.position[1] >> vertex.position[2];
+            vertices.push_back(vertex);
+        }
+        else if (token == "f") {
+            // Face 
+            for (int i = 0; i < 3; i++) {
+                std::vector<Vertex>::size_type vIndex;
+                std::string vertexStr;
+                iss >> vertexStr;
 
-typedef float Flt;
-typedef Flt Vec[3];
-typedef Flt Matrix[4][4];
+                // Split the vertex string into parts using '/'
+                std::istringstream vertexStream(vertexStr);
+                std::getline(vertexStream, token, '/');
+                vIndex = std::stoi(token);
 
-// The functions below will be anle  to draw the racetrack
-void drawRacetrack() {
-    glPushMatrix();
-    glColor3f(0.2f, 0.2f, 0.2f);
-    
-    // These fucntions below will decide the width and length of the track.
-    float trackWidth = 10.0f;
-    float trackLength = 100.0f;
-    
-    glBegin(GL_QUADS);
-    glVertex3f(-trackWidth, 0.0f, -trackLength);
-    glVertex3f(trackWidth, 0.0f, -trackLength);
-    glVertex3f(trackWidth, 0.0f, trackLength);
-    glVertex3f(-trackWidth, 0.0f, trackLength);
-    glEnd();
+                std::cout << "Vertex Index: " << vIndex << std::endl;
 
-    glPopMatrix();
+                if (vIndex <= vertices.size()) {
+                    // Valid vertex index
+                    mesh.vertices.push_back(vertices[vIndex - 1]);
+                    //mesh.indices.push_back(vIndex - 1);
+                    mesh.indices.push_back(faceIndex++);
+                }
+            }
+        }
+
+    }
+    return mesh;
+}
+//Mouse Timer functions.
+int time_since_mouse_moved(const bool get)
+{
+static std::chrono::steady_clock::time_point lastMouseMoveTime = std::chrono::steady_clock::now();
+if (!get) {
+// Update the time
+        lastMouseMoveTime = std::chrono::steady_clock::now();
+        return 0;
+    }
+    // Calculate the time difference
+    std::chrono::steady_clock::time_point now = std::chrono::steady_clock::now();
+    std::chrono::duration<double> time_span = std::chrono::duration_cast<std::chrono::duration<double>>(now - lastMouseMoveTime);
+    int elapsedTime = static_cast<int>(time_span.count());
+    return elapsedTime;
 }
 
 
