d25ef3f673188b4d68f7e859bff79dc028881a1f
Mostafa Abadi <mabadi@odin.cs.csubak.edu>
2023-11-18 22:02:35 -0800

    All files to implement the racetrack

---
+++ azurita.h
@@ -0,0 +1,20 @@
+//Alexis Zurita
+//Only for declaration of function, Not for executables
+//
+
+#ifndef _azurita_H_
+#define _azurita_H_
+
+#include <iostream>
+#include <ctime>
+#include <chrono>
+#include <GL/gl.h>
+#include <GL/glx.h>
+#include <GL/glu.h>
+
+
+void drawAzuritaCar();
+int total_time_since_key_press(const bool get);
+
+
+#endif
