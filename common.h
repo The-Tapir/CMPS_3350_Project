1452d4c1fb5f3852d68b7d36d0735f622226a0af
student <dayeni@csub.edu>
2023-12-07 22:19:29 -0800

    common header file

---
+++ common.h
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <chrono>
+#include <GL/gl.h>
+#include <GL/glx.h>
+#include <GL/glu.h>
+#include <cmath>
+
+//#include <unistd.h>
+//#include <time.h>
+#include <math.h>
+#include <X11/Xlib.h>
+//#include <X11/Xutil.h>
+#include <X11/keysym.h>
+#include <GL/gl.h>
+#include <GL/glx.h>
+#include <GL/glu.h>
+#include "log.h"
+#include "fonts.h"
+#include "nwardinsky.h"
+#include "mabadi.h"
+
+#include "dayeni.h"
+#include "azurita.h"
+#include <string>
+/*
+typedef float Flt;
+typedef Flt Vec[3];
+typedef Flt	Matrix[4][4];
+
+//Variables added by Nathan
+extern int timed = 0;
+extern bool show_stats = false;
+//Variables added by David
+extern int selectedOption = 0;// 0 represents "Play" as the default selection
+extern enum GameState { MENU, PLAY, HIGHSCORE, EXIT};
+extern float Pos1;
+extern float Pos2;
+extern float Pos3;
+extern float theta;
+*/
+
